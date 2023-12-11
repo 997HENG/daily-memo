ed25cc04aa902b975d6836faa910412f2aa51a37
997heng <997heng@gamil.com>
2023-12-12 07:54:43 +0800

    done

--- app/src/main/java/com/example/daily_memo/MainActivity.java
+++ app/src/main/java/com/example/daily_memo/MainActivity.java
@@ -1,11 +1,19 @@
 package com.example.daily_memo;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -13,15 +21,18 @@ public class MainActivity extends AppCompatActivity {
     private Button login;
     private Button register;
     private FirebaseAuth auth;
+    private ImageView logo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
-
+        logo = findViewById(R.id.logo);
         login = findViewById(R.id.toLogin);
         register = findViewById(R.id.toRegister);
+        registerForContextMenu(logo);
 
         auth = FirebaseAuth.getInstance();
 
@@ -52,6 +63,50 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        MenuInflater inflater = new MenuInflater(this);
+        inflater.inflate(R.menu.menu,menu);
+        menu.setHeaderTitle("Daily Memo");
+
+
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.about:{
+                new AlertDialog.Builder(MainActivity.this).setTitle("about").setMessage("author:Heng\nid:U1024026").setPositiveButton(
+                        "OK",new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.dismiss();
+                            }
+                        }
+                ).show();
+            }
+            break;
+            case R.id.github:{
+                new AlertDialog.Builder(MainActivity.this).setTitle("github").setMessage("name:997heng\nrepository:https://github.com/997HENG/daily-memo").setPositiveButton(
+                        "OK",new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri.parse("https://github.com/997HENG/daily-memo")));
+                                dialogInterface.dismiss();
+                            }
+                        }
+                ).show();
+            }
+            break;
+            case R.id.exit:{
+                finish();
+                break;
+            }
+        }
+        return super.onContextItemSelected(item);
+    }
+
     public boolean isLogin(){
         return auth.getCurrentUser() != null;
     }
